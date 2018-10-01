5ca91bccf582312fb1c30c9bb4028e83d1dafcaf
Martin Spencer <m.spencer@1xinternet.de>
2018-10-01 12:50:47 +0200

    fix(core): createStore returns a promise

--- packages/core/src/utils/createStore.js
+++ packages/core/src/utils/createStore.js
@@ -7,7 +7,7 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { reducer as api, setAxiosConfig } from "redux-json-api";
 
-export const createStore = ({
+export const createStore = async ({
   reducers = {},
   middlewares = [],
   enhancers = [],
@@ -15,49 +15,43 @@ export const createStore = ({
   apiEndpoint,
   loadCSRFToken = true
 }) => {
-  if (!apiEndpoint) {
-    throw new Error(
-      "You will need to call createStore with an `apiEndpoint` pointing to the publicly accessible URL to your JSON:API!"
+  try {
+    if (!apiEndpoint) {
+      throw new Error(
+        "You will need to call createStore with an `apiEndpoint` pointing to the publicly accessible URL to your JSON:API!"
+      );
+    }
+    if (loadCSRFToken && !csrfTokenEndpoint) {
+      throw new Error(
+        "Either call createStore with `loadCSRFToken: false ` or provide an `csrfTokenEndpoint`!"
+      );
+    }
+
+    const reducer = combineReducers({
+      api,
+      ...reducers
+    });
+
+    const store = createReduxStore(
+      reducer,
+      composeWithDevTools(applyMiddleware(thunk, ...middlewares), ...enhancers)
     );
-  }
-  if (loadCSRFToken && !csrfTokenEndpoint) {
-    throw new Error(
-      "Either call createStore with `loadCSRFToken: false ` or provide an `csrfTokenEndpoint`!"
-    );
-  }
 
-  const reducer = combineReducers({
-    api,
-    ...reducers
-  });
+    if (loadCSRFToken) {
+      const token = await fetch(csrfTokenEndpoint).then(res => res.text());
+    }
 
-  const store = createReduxStore(
-    reducer,
-    composeWithDevTools(applyMiddleware(thunk, ...middlewares), ...enhancers)
-  );
-
-  const initialiseApi = token =>
     store.dispatch(
       setAxiosConfig({
         baseURL: apiEndpoint,
-        ...(token
-          ? {
-              headers: {
-                "X-CSRF-Token": token
-              }
-            }
-          : {})
+        headers: {
+          ...(token ? { "X-CSRF-Token": token } : {})
+        }
       })
     );
 
-  // Initialize API with a token if we have the parameters for it.
-  if (loadCSRFToken) {
-    fetch(csrfTokenEndpoint)
-      .then(res => res.text())
-      .then(token => initialiseApi);
-  } else {
-    initialiseApi();
+    return store;
+  } catch (e) {
+    console.error("Error creating editable store!", e);
   }
-
-  return store;
 };
