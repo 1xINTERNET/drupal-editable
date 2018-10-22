01b395064be1280ac6ac78b82be97ca9c46c7cdf
Martin Spencer <m.spencer@1xinternet.de>
2018-10-22 15:03:52 +0200

    fix: added typescript babel preset to global babel config for tests

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   babelrcRoots: "packages/**",
-  presets: ["@babel/preset-react", "@babel/preset-env"],
+  presets: [
+    "@babel/preset-react",
+    "@babel/preset-env",
+    "@babel/preset-typescript"
+  ],
   plugins: [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-syntax-export-default-from"
